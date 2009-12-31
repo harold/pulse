b498ae9903bc9674910ec94658766a8472283eb7
Harold <hhausman@gmail.com>
2009-12-30 18:17:36 -0700

    fixed width for sparklines

--- public/index.js
+++ public/index.js
@@ -1,11 +1,12 @@
 jQuery(function($){
+	var theChartOptions = {chartRangeMin:1, chartRangeMax:5, width:"200px"}
 	$("*:radio").click(function(evt){
 		var theName = this.name;
 		$.post("/answer",{question_id:this.name, value:this.value},
 			function(data){
-				$("#graph-"+theName).sparkline(data.values,{chartRangeMin:1, chartRangeMax:5});
+				$("#graph-"+theName).sparkline(data.values,theChartOptions);
 			},"json");
 	});
 	
-	$(".graph").sparkline('html',{chartRangeMin:1, chartRangeMax:5});
+	$(".graph").sparkline('html',theChartOptions);
 });
