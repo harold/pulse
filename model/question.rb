b7667c79084c20c3ca849fb2248b19ecf8fdf68d
Gavin Kistner <gavin@phrogz.net>
2010-01-01 15:26:27 -0700

    Update Question model to allow eager loading for timestamped answers; foundation for querying about hidden status

--- model/question.rb
+++ model/question.rb
@@ -1,8 +1,14 @@
 class Question < Sequel::Model
 	many_to_one :user
 	one_to_many :answers, :order=>:epoch
+	one_to_many :question_prefs
+	
 	def timestamped_answers( user_id )
-		answers_dataset.filter( :user_id=>user_id ).select(:epoch,:value).map{ |a| [a.epoch,a.value] }
+		answers.select{ |a| a.user_id==user_id }.map{ |a| [a.epoch,a.value] }
+	end
+	def hidden_for?( user_id )
+		pref = question_prefs.select{ |qp| qp.user_id==user_id }.first
+		pref && pref[ :priority ] < 0
 	end
 end
 
