469230d01af08c15e654bae42906fa6bf7957937
Gavin Kistner <gavin@phrogz.net>
2010-01-01 13:10:08 -0700

    Lower update window from 60 seconds to 45

--- model/answer.rb
+++ model/answer.rb
@@ -1,7 +1,8 @@
 class Answer < Sequel::Model
-	# Number of seconds below which to count a new answer as an update instead
-	UPDATE_WINDOW = 60 * 1
 	many_to_one :question
+
+	# Number of seconds below which to count a new answer as an update instead
+	UPDATE_WINDOW = 45
 
 	def self.update_or_create( values, update_window=UPDATE_WINDOW )
 		# TODO: Instead of deleting and re-adding a new record, is there any good reason to perhaps actually update the last record(s)?
