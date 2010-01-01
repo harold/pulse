5dd276a6b1ec36c1f055f8c917d23664500cf19d
Gavin Kistner <gavin@phrogz.net>
2009-12-31 20:25:19 -0700

    New answers to the same question within 60s of the last will automatically 'update' the answer (delete/re-add)

--- model/answer.rb
+++ model/answer.rb
@@ -1,3 +1,11 @@
 class Answer < Sequel::Model
+	# Number of seconds below which to count a new answer as an update instead
+	UPDATE_WINDOW = 60 * 1
 	many_to_one :question
+
+	def before_create
+		similar = self.class.filter( :user_id=>self.user_id, :question_id=>self.question_id )
+		similar.filter( 'epoch >= ?', self.epoch - UPDATE_WINDOW ).delete
+		super
+	end
 end
